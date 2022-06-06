1878a09d07c6af8f22c4d1eeb178a390cff6dcb5
faithhub <amaofaith.o@gmail.com>
2022-06-06 20:35:45 +0100

    update from webhook

--- src/controllers/webhook.controller.js
+++ src/controllers/webhook.controller.js
@@ -22,25 +22,6 @@ module.exports = class {
     try {
       const body = req.body;
       const object = req.body.object;
-      //   if (object == "whatsapp_business_account") {
-      //     const info = {
-      //       phone: "message.from",
-      //       text: "message.text.body",
-      //       others: object,
-      //     };
-      //     await Messages.create(info);
-
-      //     // const messages = object.entry[0].changes[0].value.messages;
-      //     // messages.map((message) => {
-      //     //   const info = {
-      //     //     phone: message.from,
-      //     //     text: message.text.body,
-      //     //     others: message.text,
-      //     //   };
-      //     //   Messages.create(info).then().catch();
-      //     // });
-      //   }
-
       if (object == "whatsapp_business_account") {
         const messages = body.entry[0].changes[0].value.messages;
         messages.map((message) => {
